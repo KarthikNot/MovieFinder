bc8efe23e9c939b945887b7eaff32a4d5212f8e2
Karthikeyan's Mac <anumallakarthikeyan03@gmail.com>
2026-05-18 17:07:22 +0530

    config and logger

---
+++ src/core/config.py
@@ -0,0 +1,6 @@
+import os
+
+KAGGLE_DATASET_LINK = "ggtejas/tmdb-imdb-merged-movies-dataset"
+
+MOVIES_DATASET_PATH = os.path.join(os.getcwd(), 'data', 'raw_dataset.csv')
+PREPROCESSED_DATASET_PATH = os.path.join(os.getcwd(), 'data', 'preprocessed_dataset.csv')
